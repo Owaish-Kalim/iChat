dc250e8d35c15b55b5c5ac95f81691a3f6e7e1e1
Owaish123 <owaish.kalim@krazybee.com>
2022-03-01 23:04:32 +0530

    Added SignUp/SignIn UI + Functionality

---
+++ iChat/FCollectionReference.swift
@@ -0,0 +1,25 @@
+//
+//  FCollectionReference.swift
+//  iChat
+//
+//  Created by owaish on 01/03/22.
+//
+
+import Foundation
+import FirebaseFirestore
+
+
+enum FCollectionReference: String {
+    case User
+    case Typing
+    case Recent
+    case Message
+    case Group
+    case Call
+}
+
+
+func reference(_ collectionReference: FCollectionReference) -> CollectionReference{
+    return Firestore.firestore().collection(collectionReference.rawValue)
+}
+
